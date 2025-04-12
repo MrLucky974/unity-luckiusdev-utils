e325d4e6999abeeb4eda815890a8e1536429d460
Luckius_ <herocreepyfr@gmail.com>
2025-04-12 11:40:04 +0400

    Update log methods

--- Runtime/Utils/ComponentHelper.cs
+++ Runtime/Utils/ComponentHelper.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace LuckiusDev.Utils
@@ -11,35 +10,6 @@ namespace LuckiusDev.Utils
         public static Vector3 GetLocalPosition(this Component component) => component.transform.localPosition;
         public static Quaternion GetRotation(this Component component) => component.transform.rotation;
         public static Quaternion GetLocalRotation(this Component component) => component.transform.localRotation;
-
-        #endregion
-
-        #region Logging
-
-        public static void Log(this Component component, string message)
-        {
-            Debug.Log(message, component);
-        }
-
-        public static void LogWarning(this Component component, string message)
-        {
-            Debug.LogWarning(message, component);
-        }
-
-        public static void LogError(this Component component, string message)
-        {
-            Debug.LogError(message, component);
-        }
-
-        public static void LogAssertion(this Component component, string message)
-        {
-            Debug.LogAssertion(message, component);
-        }
-
-        public static void LogException(this Component component, Exception exception)
-        {
-            Debug.LogException(exception, component);
-        }
 
         #endregion
     }
--- Runtime/Utils/DrowsyLogger.cs
+++ Runtime/Utils/DrowsyLogger.cs
@@ -6,31 +6,52 @@ namespace LuckiusDev.Utils
 {
     public static class DrowsyLogger
     {
-        public static string Color( this string str, string color ) {
+        public static string Color( this string str, string color )
+        {
             return $"<color={color}>{str}</color>";
         }
 
-        private static void DoLog( Action<string, Object> LogFunction, string prefix, Object obj, params object[] msg ) {
+        private static void DoLog( Action<string, Object> LogFunction, string prefix, Object obj, params object[] msg )
+        {
 #if UNITY_EDITOR
             var name = ( obj ? obj.name : "NullObject" ).Color("lightblue");
             LogFunction($"{prefix}[{name}]: {string.Join("; ", msg)}\n ", obj);
 #endif
         }
 
-        public static void LogDebug( this Object obj, params object[] msg ) {
+        public static void LogDebug( this Object obj, params object[] msg )
+        {
             DoLog(Debug.Log, "", obj, msg);
         }
 
-        public static void LogError( this Object obj, params object[] msg ) {
+        public static void LogError( this Object obj, params object[] msg )
+        {
             DoLog(Debug.LogError, "X".Color("red"), obj, msg);
         }
 
-        public static void LogWarning( this Object obj, params object[] msg ) {
+        public static void LogWarning( this Object obj, params object[] msg )
+        {
             DoLog(Debug.LogWarning, "⚠️".Color("yellow"), obj, msg);
         }
 
-        public static void LogSuccess( this Object obj, params object[] msg ) {
+        public static void LogSuccess( this Object obj, params object[] msg )
+        {
             DoLog(Debug.Log, "✔️".Color("green"), obj, msg);
+        }
+
+        public static void LogInfo(this Object obj, params object[] msg)
+        {
+            DoLog(Debug.Log, "ℹ️".Color("cyan"), obj, msg);
+        }
+
+        public static void LogTrace(this Object obj, params object[] msg)
+        {
+            DoLog(Debug.Log, "🔍".Color("grey"), obj, msg);
+        }
+
+        public static void LogCritical(this Object obj, params object[] msg)
+        {
+            DoLog(Debug.LogError, "💥".Color("magenta"), obj, msg);
         }
     }
 }
