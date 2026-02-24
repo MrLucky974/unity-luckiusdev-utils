c22b3b4536f79f1b25ee35758b67d8334c0e46ae
Luckius_ <herocreepyfr@gmail.com>
2026-02-24 14:52:05 +0400

    Refactor extension classes
    - Move TryGetComponentAnywhere from GameObjectExtensions to ComponentExtensions
    - Add GetOrCreate method to ComponentExtensions

--- Runtime/Utils/Extensions/ComponentExtensions.cs
+++ Runtime/Utils/Extensions/ComponentExtensions.cs
@@ -124,5 +124,79 @@ namespace LuckiusDev.Utils.Extensions
             component != null && component.gameObject.activeInHierarchy;
 
         #endregion
+
+        #region Search & Addition
+
+        /// <summary>
+        /// Attempts to find a component of type <typeparamref name="T"/> on the current object,
+        /// in its children (optionally including inactive ones), or up through its parent hierarchy.
+        /// </summary>
+        /// <typeparam name="T">The type of component to find.</typeparam>
+        /// <param name="obj">The starting component.</param>
+        /// <param name="component">Outputs the component if found.</param>
+        /// <param name="includeInactive">Whether to include inactive children in the search.</param>
+        /// <returns>True if the component was found; otherwise, false.</returns>
+        public static bool TryGetComponentAnywhere<T>(this Component obj, out T component, bool includeInactive = false) where T : Component
+        {
+            // Check on the current object
+            if (obj.TryGetComponent(out component))
+                return true;
+
+            // Check in children (with option to include inactive ones)
+            component = obj.GetComponentInChildren<T>(includeInactive);
+            if (component != null)
+                return true;
+
+            // Recurse up the parent hierarchy
+            var parent = obj.transform.parent;
+            while (parent != null)
+            {
+                if (parent.TryGetComponent(out component))
+                    return true;
+
+                parent = parent.parent;
+            }
+
+            component = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Overload for GameObject to call TryGetComponentAnywhere in the same way.
+        /// </summary>
+        public static bool TryGetComponentAnywhere<T>(this GameObject gameObject, out T component,
+            bool includeInactive = false) where T : Component
+        {
+            return gameObject.transform.TryGetComponentAnywhere(out component, includeInactive);
+        }
+
+        /// <summary>
+        /// Gets a component of type T from the GameObject if it exists,
+        /// otherwise adds the component and returns the newly created instance.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The type of Component to retrieve or add. Must inherit from Component.
+        /// </typeparam>
+        /// <param name="gameObject">
+        /// The GameObject on which to get or create the component.
+        /// </param>
+        /// <returns>
+        /// The existing component if found; otherwise, a newly added component of type T.
+        /// </returns>
+        /// <remarks>
+        /// This method avoids calling AddComponent if the component already exists,
+        /// making it safe and efficient to ensure a required component is present.
+        /// </remarks>
+        public static T GetOrCreate<T>(this GameObject gameObject) where T : Component
+        {
+            if (gameObject.TryGetComponent<T>(out var component))
+            {
+                return component;
+            }
+
+            return gameObject.AddComponent<T>();
+        }
+
+        #endregion
     }
 }
